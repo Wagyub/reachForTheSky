1bd065933e210e47359ecba64c409efb98ee7884
Fabian <fabiansantiago467@gmail.com>
2025-08-22 00:56:13 +0200

    Began working on pawn and player, added 3D model

---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+
+    public Vector3 pawnPosition = Vector3.zero;
+
+    /*void pawnMove()
+    {
+        transform.position = transform.position + ajouter position de la cellule
+    }*/
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
